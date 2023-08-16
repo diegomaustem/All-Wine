40e4fbbbc74f1e8cf97ca4ee963386f574df4c65
diego <diego.maustem@gmail.com>
2023-08-16 08:27:06 -0300

    :sparkles: Implementando o graphql

--- All-Wine/routes/api.php
+++ All-Wine/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('wines',[BicycleController::class, 'getAllWines']);
+Route::post('wines',[BicycleController::class, 'createBicycles']);
